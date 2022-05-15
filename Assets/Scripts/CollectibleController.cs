1aa3fe08862c46d0109fc93d315792608a73dc50
KingDee241 <57509042+KingDee241@users.noreply.github.com>
2022-05-15 12:23:17 -0400

    added Level End item and level end UI

--- Assets/Scripts/CollectibleController.cs
+++ Assets/Scripts/CollectibleController.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class CollectibleController : MonoBehaviour
 {
+    [SerializeField] GameObject levelEndController;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<PlayerController>().collectCoin();
+            if (this.gameObject.CompareTag("Objective"))
+            {
+                levelEndController.GetComponent<CanvasController>().LevelEnd();
+            }
+            else collision.gameObject.GetComponent<PlayerController>().collectCoin();
+            
             Destroy(gameObject);
         }
     }
