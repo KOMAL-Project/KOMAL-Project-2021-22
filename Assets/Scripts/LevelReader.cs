9c0618ec9735fb3ade2c8df578e79308524e2237
KingDee241 <57509042+KingDee241@users.noreply.github.com>
2022-06-15 16:55:09 -0400

    changed player hitbox, added Cave rule tile, implemented basic new map

--- Assets/Scripts/LevelReader.cs
+++ Assets/Scripts/LevelReader.cs
@@ -29,6 +29,20 @@ public class LevelReader : MonoBehaviour
 
     public GameObject tileMapObj;
 
+    // Level Color Definitions:
+    // Terrain Colors
+    Color platformColor = new Color(0,0,0);
+    Color caveColor = new Color(64f / 255f, 64f / 255f, 64f / 255f);
+    Color grassColor = new Color(182f / 255f, 255f / 255f, 0);
+    Color sandColor = new Color(255f / 255f, 216f / 255f, 0);
+    // Prefab Colors
+    Color crabColor = new Color(255f / 255f, 127f / 255f, 0);
+    Color megaCrabColor = new Color(222f / 255f, 107f / 255f, 0);
+    Color umbrellaColor = new Color(0, 0, 1);
+    Color checkpointColor = new Color(1, 0, 0);
+    Color collectibleColor = new Color(178f / 255f, 0, 1);
+    Color objectiveColor = new Color(1, 0 , 110f / 255f);
+
     void Start()
     {
         tiles = tileMapObj.GetComponent<Tilemap>();
@@ -44,57 +58,54 @@ public class LevelReader : MonoBehaviour
             {
                 Color tempColor = level.GetPixel(i,j);
 
-                if (tempColor == Color.black)
-                {
-                    /*if (i % 2 == 0 && j % 2 == 0) tiles.SetTile(new Vector3Int(i, j, 0), tileList[0]);
-                    else if (i % 2 == 1 && j % 2 == 0) tiles.SetTile(new Vector3Int(i, j, 0), tileList[1]);
-                    else if (i % 2 == 0 && j % 2 == 1) tiles.SetTile(new Vector3Int(i, j, 0), tileList[2]);
-                    else tiles.SetTile(new Vector3Int(i, j, 0), tileList[3]);*/
-
-                    if (groundType == GroundType.TEST)
-                    { //TEST
-                        tiles.SetTile(new Vector3Int(i, j, 0), ruleTiles[0]);
-                    }
-                    else if (groundType == GroundType.GRASS)
-                    { //GRASS
-                        tiles.SetTile(new Vector3Int(i, j, 0), ruleTiles[1]);
-                    }
-                    else 
-                    { //SAND
-                        tiles.SetTile(new Vector3Int(i, j, 0), ruleTiles[2]);
-                    }
+                
+                if (tempColor == platformColor)
+                { //TEST
+                    tiles.SetTile(new Vector3Int(i, j, 0), ruleTiles[0]);
                 }
-                else if (tempColor == Color.cyan && umbrellaPrefab) 
+                else if (tempColor == grassColor)
+                { //GRASS
+                    tiles.SetTile(new Vector3Int(i, j, 0), ruleTiles[1]);
+                }
+                else if(tempColor == sandColor)
+                { //SAND
+                    tiles.SetTile(new Vector3Int(i, j, 0), ruleTiles[2]);
+                }
+                else if(tempColor == caveColor)
+                { //CAVE
+                    tiles.SetTile(new Vector3Int(i, j, 0), ruleTiles[3]);
+                }
+                // PREFAB CHECKS
+                else if (tempColor == umbrellaColor && umbrellaPrefab) 
                 {
                     //tiles.SetTile(new Vector3Int(i, j, 0), tileList[4]);
 
                     GameObject tempUmbrella = Instantiate(umbrellaPrefab, tiles.GetCellCenterWorld(new Vector3Int(i, j+1, 0)), Quaternion.identity);
                     umbrellas.Add(tempUmbrella);
                 }
-                else if(tempColor == Color.red)
+                else if(tempColor == checkpointColor)
                 {
                     GameObject tempCheckpoint = Instantiate(checkpointPrefab, tiles.GetCellCenterWorld(new Vector3Int(i, j, 0)) + new Vector3(.5f, .5f, 0), Quaternion.identity);
                     checkpoints.Add(tempCheckpoint);
                 }
-                else if (tempColor == new Color(1, 127f/255f, 0))
+                else if (tempColor == crabColor)
                 {
                     GameObject tempCrab;
                     tempCrab = Instantiate(crabPrefab, tiles.GetCellCenterWorld(new Vector3Int(i, j, 0)), Quaternion.identity);
                     crabs.Add(tempCrab);
                 }
-                else if(tempColor == new Color(222f / 255f, 107f / 255f, 0))
+                else if(tempColor == megaCrabColor)
                 {
-                    Color megaCrabbish = new Color(222f / 255f, 107f / 255f, 0);
                     GameObject tempCrab;
-                    if (level.GetPixel(i + 1, j + 1) == megaCrabbish && level.GetPixel(i, j + 1) == megaCrabbish && level.GetPixel(i + 1, j) == megaCrabbish) tempCrab = Instantiate(megaCrabPrefab, tiles.GetCellCenterWorld(new Vector3Int(i, j, 0)) + new Vector3(0, 1, 0), Quaternion.identity);
+                    if (level.GetPixel(i + 1, j + 1) == megaCrabColor && level.GetPixel(i, j + 1) == megaCrabColor && level.GetPixel(i + 1, j) == megaCrabColor) tempCrab = Instantiate(megaCrabPrefab, tiles.GetCellCenterWorld(new Vector3Int(i, j, 0)) + new Vector3(0, 1, 0), Quaternion.identity);
                 
                 }
-                else if (tempColor == new Color(1,1,0))
+                else if (tempColor == collectibleColor)
                 {
                     GameObject tempCollect = Instantiate(collectPrefab, tiles.GetCellCenterWorld(new Vector3Int(i, j, 0)) + new Vector3(0, .5f, 0), Quaternion.identity);
                     
                 }
-                else if (tempColor == Color.green)
+                else if (tempColor == objectiveColor)
                 {
                     GameObject tempGoal = Instantiate(goalPrefab, tiles.GetCellCenterWorld(new Vector3Int(i, j, 0)) + new Vector3(.5f, .5f, 0), Quaternion.identity);
                     goal = tempGoal;
